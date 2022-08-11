37651b5532d86d8dc2bba6e354ef38ea64a9820c
Gonzalo Cruz <cruzgonzalo7@gmail.com>
2022-08-11 17:37:52 -0300

    creacion del proyecto + instalacion de stack navigator

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import {Text} from 'react-native';
+import React from 'react';
+
+const LoginScreen = () => {
+  return (
+    <>
+      <Text>LoginScreen</Text>
+    </>
+  );
+};
+
+export default LoginScreen;
