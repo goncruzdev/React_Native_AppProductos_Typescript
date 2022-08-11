d8201bd3d949cb8faa2e7a2c6cbfc80fa7b2b9d7
Vito Bernardo Bernal <vito.bernal@infocorpgroup.com>
2022-08-11 18:43:39 -0300

    creo base de pantalla de login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,10 +1,21 @@
-import {Text} from 'react-native';
+import {Text, TextInput} from 'react-native';
 import React from 'react';
+import Background from '../components/Background';
+import WhiteLogo from '../components/WhiteLogo';
+import {loginStyles} from '../theme/loginTheme';
 
 const LoginScreen = () => {
   return (
     <>
-      <Text>LoginScreen</Text>
+      <Background />
+      <WhiteLogo />
+      <Text style={loginStyles.title}>Login</Text>
+      <Text style={loginStyles.label}>Email</Text>
+      <TextInput
+        placeholder="Ingrese su email"
+        placeholderTextColor="#rgba(255,255,255,0.4)"
+        keyboardType="email-address"
+      />
     </>
   );
 };
